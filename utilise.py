f649661a5d5b55150022dbecf6fdbd0f2fc2538c
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-16 13:38:04 +0530

    getting smooth edges

--- utilise.py
+++ utilise.py
@@ -4,4 +4,9 @@ def getContoutrs(img,cThr=[100,100],showCanny= False):
     imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
     imgCanny = cv2.Canny(imgBlur, cThr[0],cThr[1])
-    if showCanny:cv2.imshow('Canny',imgCanny)
+    kernel= np.ones((5,5))
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=3)
+    imgThre = cv2.erode(imgDial,kernel,iterations=2)
+
+
+    if showCanny:cv2.imshow('Canny',imgThre)
